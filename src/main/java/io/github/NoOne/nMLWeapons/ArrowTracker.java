d269416c19678e1c5517bd69b0fadd6862efd07d
NoOne_NoBody <hypixelnerd347@gmail.com>
2026-09-19 13:47:59 -0400

    added methods for adding a trail and despawn timer for arrows

--- src/main/java/io/github/NoOne/nMLWeapons/ArrowTracker.java
+++ src/main/java/io/github/NoOne/nMLWeapons/ArrowTracker.java
@@ -16,6 +16,8 @@ public class ArrowTracker {
     private static NMLWeapons nmlWeapons = NMLWeapons.getInstance();
     public static ArrayList<Arrow> flyingArrows = new ArrayList<>();
     public static HashMap<Arrow, Integer> arrowTimers = new HashMap<>();
+    public static HashMap<Arrow, Integer> arrowTimeSaves = new HashMap<>(); // for arrows that have a preset despawn time
+    public static ArrayList<Arrow> noTrailArrows = new ArrayList<>(); // for arrows that won't have a trail
 
     public static void startArrowTracker() {
         new BukkitRunnable() {
@@ -24,18 +26,18 @@ public class ArrowTracker {
             public void run() {
                 Iterator<Arrow> flyingArrowsIterator = flyingArrows.iterator();
 
-                while (flyingArrowsIterator.hasNext()) {
+                while (flyingArrowsIterator.hasNext()) { // dead arrow check
                     Arrow arrow = flyingArrowsIterator.next();
 
                     if (arrow.isDead() || arrow.isInBlock()) {
-                        arrowTimers.put(arrow, 40);
+                        arrowTimers.put(arrow, arrowTimeSaves.getOrDefault(arrow, 80));
                         flyingArrowsIterator.remove();
                     }
                 }
 
                 Iterator<Map.Entry<Arrow, Integer>> arrowTimerIterator = arrowTimers.entrySet().iterator();
 
-                while (arrowTimerIterator.hasNext()) {
+                while (arrowTimerIterator.hasNext()) { // arrow despawn check
                     Map.Entry<Arrow, Integer> entry = arrowTimerIterator.next();
                     int time = entry.getValue();
 
@@ -46,7 +48,7 @@ public class ArrowTracker {
                     }
                 }
             }
-        }.runTaskTimer(nmlWeapons, 0, 2);
+        }.runTaskTimer(nmlWeapons, 0, 1);
     }
 
     public static void startArrowTrailTracker() {
@@ -54,12 +56,14 @@ public class ArrowTracker {
             @Override
             public void run() {
                 for (Arrow arrow : flyingArrows) {
-                    double speed = arrow.getVelocity().length();
-                    int particleCount = (int) (Math.pow(speed, 2) * 5);
+                    if (!noTrailArrows.contains(arrow)) {
+                        double speed = arrow.getVelocity().length();
+                        int particleCount = (int) (Math.pow(speed, 2) * 5);
 
-                    if (particleCount > 0) {
-                        Location loc = arrow.getLocation();
-                        arrow.getWorld().spawnParticle(Particle.CRIT, loc, particleCount,0, 0, 0, 0);
+                        if (particleCount > 0) {
+                            Location loc = arrow.getLocation();
+                            arrow.getWorld().spawnParticle(Particle.CRIT, loc, particleCount,0, 0, 0, 0);
+                        }
                     }
                 }
             }
@@ -70,5 +74,22 @@ public class ArrowTracker {
         arrow.setMetadata("custom_arrow", new FixedMetadataValue(nmlWeapons, damage));
         arrow.setCritical(false);
         flyingArrows.add(arrow);
+    }
+
+    public static void makeCustomArrow(Arrow arrow, HashMap<DamageType, Double> damage, boolean trail) {
+        makeCustomArrow(arrow, damage);
+
+        if (!trail) {
+            noTrailArrows.add(arrow);
+        }
+    }
+
+    public static void makeCustomArrow(Arrow arrow, HashMap<DamageType, Double> damage, boolean trail, int despawnTicks) {
+        makeCustomArrow(arrow, damage, trail);
+        arrowTimeSaves.put(arrow, despawnTicks);
+
+        if (!trail) {
+            noTrailArrows.add(arrow);
+        }
     }
 }
